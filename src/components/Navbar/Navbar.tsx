fba7d93f54a36025e2fa5e14df6f60105d0b2693
Marian Kmotorka <marian.kmotorka@quadrotech-it.com>
2020-10-30 17:53:58 +0100

    Remove unused imports

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useMemo, useState } from 'react'
+import React, { useState } from 'react'
 import { AnimatePresence } from 'framer-motion'
 import { useHistory } from 'react-router-dom'
 
