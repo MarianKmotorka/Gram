bf793828b0fc3d932a9efa1e449580b9ef0324a6
Marian Kmotorka <marian.kmotorka@quadrotech-it.com>
2020-11-07 17:42:13 +0100

    Fix infinite loading when logged out

--- src/hooks/useFirestoreDoc.ts
+++ src/hooks/useFirestoreDoc.ts
@@ -13,7 +13,7 @@ const useFirestoreDoc = <T>(
   startFetching: boolean = true
 ): [T | undefined, boolean, firebase.firestore.FirestoreError | undefined] => {
   const [doc, setDoc] = useState<T>()
-  const [loading, setLoading] = useState(true)
+  const [loading, setLoading] = useState(false)
   const [error, setError] = useState<firebase.firestore.FirestoreError | undefined>()
 
   const onNext = (
