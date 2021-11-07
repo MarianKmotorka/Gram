3b6175739fa904ba5786ff2ff46391076eb8ab3c
Marian Kmotorka <Marian.Kmotorka@quest.com>
2021-11-07 10:14:25 +0100

    pure css top filter

--- src/pages/Feed/TopMenu/TopMenu.styled.tsx
+++ src/pages/Feed/TopMenu/TopMenu.styled.tsx
@@ -5,45 +5,46 @@ export const Wrapper = styled.div`
   justify-content: center;
   background-color: ${({ theme }) => theme.bg2};
   border-radius: 8px;
+  height: 60px;
+  position: relative;
+  z-index: 1;
+
+  .filter {
+    display: flex;
+    align-items: center;
+
+    :hover ~ span {
+      display: block;
+    }
+  }
+
+  .allFilter:hover ~ span {
+    transform: translateX(181px);
+    width: 48px;
+  }
+  .followedFilter:hover ~ span {
+    transform: translateX(249px);
+    width: 90px;
+  }
+  .mineFilter:hover ~ span {
+    transform: translateX(360px);
+    width: 58px;
+  }
+
   @media only screen and (max-width: 400px) {
     border-radius: 0px;
   }
 `
 
-export const ButtonsContainer = styled.div`
-  display: flex;
-  justify-content: center;
-  background-color: ${({ theme }) => theme.bg2};
-  height: 60px;
-  position: relative;
-  z-index: 1;
-
-  > div {
-    display: flex;
-    align-items: center;
-  }
-`
-
-interface IndicatorProps {
-  offset: number
-  width: number
-  isHidden: 0 | 1
-}
-
-export const Indicator = styled.span<IndicatorProps>`
+export const Indicator = styled.span`
   height: 32px;
-  width: 70px;
-  display: block;
+  display: none;
   background: ${({ theme }) => theme.accent};
   border-radius: 10px;
   position: absolute;
   z-index: -1;
-
   left: 0;
   top: 14px;
   opacity: 0.6;
-  display: ${({ isHidden }) => (isHidden ? 'none' : 'block')};
   transition: transform 0.25s, width 0.25s;
-  transform: ${({ offset }) => `translateX(${offset}px)`};
-  width: ${({ width }) => `${width}px`};
 `
--- src/pages/Feed/TopMenu/TopMenu.tsx
+++ src/pages/Feed/TopMenu/TopMenu.tsx
@@ -1,10 +1,9 @@
-import React, { FC, useEffect, useState } from 'react'
+import React, { FC } from 'react'
 
 import { FeedFilter } from '../utils'
 import { Button } from '../../../components'
-import useHover from '../../../hooks/useHover'
 
-import { ButtonsContainer, Indicator, Wrapper } from './TopMenu.styled'
+import { Indicator, Wrapper } from './TopMenu.styled'
 
 interface ITopMenuProps {
   feedType: FeedFilter
@@ -12,75 +11,45 @@ interface ITopMenuProps {
 }
 
 const TopMenu: FC<ITopMenuProps> = ({ feedType, onChange }) => {
-  const [buttonsContainerRef, isHovered] = useHover<HTMLDivElement>()
-  const [allBtnRef, isAllBtnHovered] = useHover<HTMLDivElement>()
-  const [followedBtnRef, isFollowedBtnHovered] = useHover<HTMLDivElement>()
-  const [mineBtnRef, isMineBtnHovered] = useHover<HTMLDivElement>()
-  const [lastHovered, setLastHovered] = useState<FeedFilter>('all')
-
-  const getIndicatorOffset = () => {
-    if (lastHovered === 'all') return 7
-    if (lastHovered === 'followed') return 74
-    return 186
-  }
-
-  const getIndicatorWidth = () => {
-    if (lastHovered === 'all') return 48
-    if (lastHovered === 'followed') return 90
-    return 58
-  }
-
-  useEffect(() => {
-    if (isAllBtnHovered) setLastHovered('all')
-    if (isFollowedBtnHovered) setLastHovered('followed')
-    if (isMineBtnHovered) setLastHovered('mine')
-  }, [isAllBtnHovered, isFollowedBtnHovered, isMineBtnHovered])
-
   return (
     <Wrapper>
-      <ButtonsContainer ref={buttonsContainerRef}>
-        <div ref={allBtnRef}>
-          <Button
-            hover={false}
-            scale={0.8}
-            color={feedType === 'all' ? 'accent2' : 'white'}
-            onClick={() => onChange('all')}
-            bg='transparent'
-          >
-            ALL
-          </Button>
-        </div>
+      <div className='allFilter filter'>
+        <Button
+          hover={false}
+          scale={0.8}
+          color={feedType === 'all' ? 'accent2' : 'white'}
+          onClick={() => onChange('all')}
+          bg='transparent'
+        >
+          ALL
+        </Button>
+      </div>
 
-        <div ref={followedBtnRef}>
-          <Button
-            hover={false}
-            scale={0.8}
-            color={feedType === 'followed' ? 'accent2' : 'white'}
-            onClick={() => onChange('followed')}
-            bg='transparent'
-          >
-            FOLLOWED
-          </Button>
-        </div>
+      <div className='followedFilter filter'>
+        <Button
+          hover={false}
+          scale={0.8}
+          color={feedType === 'followed' ? 'accent2' : 'white'}
+          onClick={() => onChange('followed')}
+          bg='transparent'
+        >
+          FOLLOWED
+        </Button>
+      </div>
 
-        <div ref={mineBtnRef}>
-          <Button
-            hover={false}
-            scale={0.8}
-            color={feedType === 'mine' ? 'accent2' : 'white'}
-            onClick={() => onChange('mine')}
-            bg='transparent'
-          >
-            MINE
-          </Button>
-        </div>
+      <div className='mineFilter filter'>
+        <Button
+          hover={false}
+          scale={0.8}
+          color={feedType === 'mine' ? 'accent2' : 'white'}
+          onClick={() => onChange('mine')}
+          bg='transparent'
+        >
+          MINE
+        </Button>
+      </div>
 
-        <Indicator
-          isHidden={isHovered ? 0 : 1}
-          width={getIndicatorWidth()}
-          offset={getIndicatorOffset()}
-        />
-      </ButtonsContainer>
+      <Indicator />
     </Wrapper>
   )
 }
